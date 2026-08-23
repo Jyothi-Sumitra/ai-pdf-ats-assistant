0e5bbac2fb2458fdaf66eb345a336bd6fd8e47ff
Jyothi-Sumitra <2400032395@kluniversity.in>
2026-08-23 14:07:30 +0530

    Fix PDF upload script

--- static/script.js
+++ static/script.js
@@ -17,7 +17,10 @@ async function checkServerAwake() {
 }
 
 // Call this as soon as the page loads
+<<<<<<< HEAD
 
+=======
+>>>>>>> 4f7b8ad (Fix PDF upload script)
 window.addEventListener("DOMContentLoaded", () => {
   const banner = document.createElement("div");
   banner.id = "wake-banner";
